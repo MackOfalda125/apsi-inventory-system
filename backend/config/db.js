79765ee2ee8b0b7c7fea76f4107410c89ed5037e
MackOfalda125 <ofaldamackenzie@gmail.com>
2025-09-13 04:51:03 +0800

    Change .env from variables to url

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,19 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
+// const pool = new Pool({
+//   host: process.env.PG_HOST,
+//   port: process.env.PG_PORT,
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PASSWORD,
+//   database: process.env.PG_DATABASE,
+//   ssl: { rejectUnauthorized: false }, // Needed for Supabase
+// });
+
 const pool = new Pool({
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
-  ssl: { rejectUnauthorized: false }, // Needed for Supabase
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // still needed for Supabase
 });
 
 module.exports = pool;
 
-// STRIP INFO BEFORE PUSHING TO GITHUB
